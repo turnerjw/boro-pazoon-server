bbb0b65e106dd56d5dff20cfce5a3e4dc3543808
Justin William Turner <jturner@spscommerce.com>
2018-09-19 15:23:23 -0400

    Implemented add user event

--- server.js
+++ server.js
@@ -11,8 +11,29 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+var numUsers = 0;
+
 io.on("connection", function(socket) {
+    var addedUser = false;
+
     console.log("User Connected");
+
+    // when the client emits 'add user', this listens and executes
+    socket.on("add user", username => {
+        if (addedUser || numUsers > 25) return;
+
+        // we store the username in the socket session for this client
+        socket.username = username;
+        ++numUsers;
+        addedUser = true;
+
+        // echo globally (all clients) that a person has connected
+        socket.broadcast.emit("user joined", {
+            username: socket.username,
+            numUsers: numUsers
+        });
+    });
+
     socket.on("disconnect", function() {
         console.log("User disconnected");
     });
